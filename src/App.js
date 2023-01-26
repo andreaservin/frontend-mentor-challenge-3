7e10dd2840164d71487b46a17db2a330807469c3
Andrea <andiserm@gmail.com>
2023-01-25 22:06:53 -0300

    Added changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {
+  Alert,
+  Button,
+  Card,
+  OverlayTrigger,
+  Spinner,
+  Tooltip,
+} from 'react-bootstrap'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const API_URL = 'https://api.adviceslip.com/advice' // this must be into .env file
+  const [advice, setAdvice] = useState(null)
+  const [error, setError] = useState(null)
+
+  const handleGetAdvice = () => {
+    axios
+      .get(API_URL)
+      .then((response) => {
+        setAdvice(response.data.slip)
+      })
+      .catch((error) => {
+        setError(error.message)
+      })
+  }
+
+  useEffect(() => {
+    handleGetAdvice()
+  }, [])
+
   return (
-    <div className="App">
-      <p>Advice generator app</p>
+    <div className='App'>
+      {error && <Alert variant='danger'>{error}</Alert>}
+
+      <Card style={{ width: '24rem', backgroundColor: 'hsl(217, 19%, 24%)' }}>
+        {advice ? (
+          <Card.Body className='text-center'>
+            <Card.Subtitle className='mb-3 subcolor'>
+              A D V I C E # {advice.id}
+            </Card.Subtitle>
+            <Card.Text className='advice'>"{advice.advice}"</Card.Text>
+          </Card.Body>
+        ) : (
+          <div className='spinner'>
+            <Spinner size='sm' animation='grow' variant='light' />
+            <Spinner size='sm' animation='grow' variant='success' />
+            <Spinner size='sm' animation='grow' variant='light' />
+          </div>
+        )}
+        <Card.Footer className='d-flex justify-content-center'>
+          <OverlayTrigger
+            placement='bottom'
+            overlay={<Tooltip>Reload</Tooltip>}
+          >
+            <Button
+              className='d-flex justify-content-center'
+              variant='success'
+              onClick={handleGetAdvice}
+            >
+              <img height={18} src='/dados.png' alt='refresh' />
+            </Button>
+          </OverlayTrigger>
+        </Card.Footer>
+      </Card>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Advice.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Advice = ({ advice }) => {
+  return <div>Advice!</div>
+}
+
+export default Advice
